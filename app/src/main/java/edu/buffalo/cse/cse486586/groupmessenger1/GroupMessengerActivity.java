2c8d9f5768a896f17c0524166e6a9207f9ee291b
Debika@UB2015 <debikadutt@gmail.com>
2016-10-31 13:16:36 -0400

    Final project submission

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger1/GroupMessengerActivity.java
@@ -0,0 +1,184 @@
+package edu.buffalo.cse.cse486586.groupmessenger1;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * GroupMessengerActivity is the main Activity for the assignment.
+ *
+ * @author stevko
+ */
+public class GroupMessengerActivity extends Activity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    static final String TAG = GroupMessengerActivity.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final String REMOTE_PORT2 = "11116";
+    static final String REMOTE_PORT3 = "11120";
+    static final String REMOTE_PORT4 = "11124";
+    static final int SERVER_PORT = 10000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_messenger);
+
+        /*
+         * TODO: Use the TextView to display your messages. Though there is no grading component
+         * on how you display the messages, if you implement it, it'll make your debugging easier.
+         */
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        
+        /*
+         * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
+         * OnPTestClickListener demonstrates how to access a ContentProvider.
+         */
+        findViewById(R.id.button1).setOnClickListener(
+                new OnPTestClickListener(tv, getContentResolver()));
+        
+        /*
+         * TODO: You need to register and implement an OnClickListener for the "Send" button.
+         * In your implementation you need to get the message from the input box (EditText)
+         * and send it to other AVDs.
+         */
+        final EditText text = (EditText) findViewById(R.id.editText1);
+        final Button sendButton = (Button) findViewById(R.id.button4);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = editText.getText().toString();
+                editText.setText("");
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, str, myPort);
+            }
+        });
+    }
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+
+            /*
+             * TODO: Fill in your server code that receives messages and passes them
+             * to onProgressUpdate().
+             */
+
+            try{
+                while(true){
+                    Socket server = serverSocket.accept();
+                    BufferedReader input = new BufferedReader(new InputStreamReader(server.getInputStream()));
+                    publishProgress(input.readLine());
+                    server.close();
+                }
+            } catch (IOException e){
+                Log.e(TAG, "Failed to connect");
+            }
+            return null;
+        }
+
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
+            remoteTextView.append(strReceived + "\t\n");
+            TextView localTextView = (TextView) findViewById(R.id.local_text_display);
+            localTextView.append("\n");
+
+            /*
+             * The following code creates a file in the AVD's internal storage and stores a file.
+             *
+             * For more information on file I/O on Android, please take a look at
+             * http://developer.android.com/training/basics/data-storage/files.html
+             */
+
+            String filename = "SimpleMessengerOutput";
+            String string = strReceived + "\n";
+            FileOutputStream outputStream;
+
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(string.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                Log.e(TAG, "File write failed");
+            }
+
+            return;
+        }
+    }
+
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            for (int i = 11108; i <= 11124; i+=4) {
+                try {
+
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                            Integer.parseInt(remotePort));
+
+                    String msgToSend = msgs[0];
+                /*
+                 * TODO: Fill in your client code that sends out a message.
+                 */
+                    PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                    output.println(msgToSend);
+                    socket.close();
+                } catch (UnknownHostException e) {
+                    Log.e(TAG, "ClientTask UnknownHostException");
+                } catch (IOException e) {
+                    Log.e(TAG, "ClientTask socket IOException");
+                }
+            }
+
+            return null;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_group_messenger, menu);
+        return true;
+    }
+}
